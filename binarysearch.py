fdd2f89f7a64af8a06ce18b9f6e95406cee5939a
jagadeshwarrao <60187298+jagadeshwarrao@users.noreply.github.com>
2020-09-06 21:13:21 +0530

    Add files via upload

---
+++ binarysearch.py
@@ -0,0 +1,35 @@
+
+def binary_search(arr, low, high, x): 
+
+	
+	if high >= low: 
+
+		mid = (high + low) // 2
+
+		
+		if arr[mid] == x: 
+			return mid 
+
+		
+		elif arr[mid] > x: 
+			return binary_search(arr, low, mid - 1, x) 
+
+	
+		else: 
+			return binary_search(arr, mid + 1, high, x) 
+
+	else: 
+	
+		return -1
+
+
+arr = [] 
+x = 10
+
+
+result = binary_search(arr, 0, len(arr)-1, x) 
+
+if result != -1: 
+	print(str(result)) 
+else: 
+	print("Element is not present in array") 
